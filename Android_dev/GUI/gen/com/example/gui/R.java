a0c79cd94fc99f3773c6c6af48df72a61069fd13
rohith reddy <rohithrockzzz@gmail.com>
2014-09-16 23:46:57 +0530

    implementing the listview and onclick search

--- Android_dev/GUI/gen/com/example/gui/R.java
+++ Android_dev/GUI/gen/com/example/gui/R.java
@@ -42,15 +42,19 @@ public final class R {
         public static final int shapes=0x7f020003;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f0a0001;
-        public static final int action_settings=0x7f0a0004;
+        public static final int Layout1=0x7f0a0001;
+        public static final int Layout2=0x7f0a0003;
+        public static final int action_settings=0x7f0a0007;
+        public static final int listview=0x7f0a0004;
         public static final int roboto=0x7f0a0002;
-        public static final int search_main=0x7f0a0003;
+        public static final int search=0x7f0a0005;
+        public static final int search_bar=0x7f0a0006;
         public static final int view_flipper=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int shape2=0x7f030001;
+        public static final int activity_search=0x7f030001;
+        public static final int shape2=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -66,6 +70,7 @@ public final class R {
         public static final int start=0x7f070003;
         public static final int title_activity_screen_slide_page_fragment=0x7f070006;
         public static final int title_activity_screen_slide_pager=0x7f070005;
+        public static final int title_activity_search=0x7f070008;
     }
     public static final class style {
         public static final int ActionBar=0x7f080002;
