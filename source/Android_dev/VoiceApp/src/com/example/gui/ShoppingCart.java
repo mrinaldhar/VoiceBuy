286a0ee24d4100c39f2422746cd904ca95ba3c4a
Bhavesh Goyal <bhavesh.goyal@students.iiit.ac.in>
2014-11-02 00:20:53 +0530

    Implemented Shopping Cart

--- source/Android_dev/VoiceApp/src/com/example/gui/ShoppingCart.java
+++ source/Android_dev/VoiceApp/src/com/example/gui/ShoppingCart.java
@@ -1,16 +1,39 @@
 package com.example.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Movie;
+import adapter.CustomListAdapter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class ShoppingCart extends Activity {
 
+
+	 public  static List<Movie> itemList = new ArrayList<Movie>();
+	 private ListView cart;
+	 private CustomListAdapter adapter;
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_shopping_cart);
+		cart = (ListView) findViewById(R.id.cart_list);
+		  adapter = new CustomListAdapter(this, itemList);
+		  cart.setAdapter(adapter);
+		  
+//          Movie movie = new Movie();
+//          movie.setTitle("Jeans");
+//          movie.setThumbnailUrl("pic");
+//          movie.setRating(Double.valueOf("1992").doubleValue());
+//          movie.setYear("1440");
+
+          // adding movie to movies array
+//          itemList.add(movie);
 	}
 
 	@Override
